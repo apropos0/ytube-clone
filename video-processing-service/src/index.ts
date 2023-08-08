e2cdf496726d9fb467dfd6e87e2a7921139d2d0d
Chloe Eimunjeze <45670453+apropos0@users.noreply.github.com>
2023-08-07 22:08:31 -0500

    Add fluent ffmpeg flow

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,28 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+app.post("/process-video", (req, res) => {
+    //request body will have path of request
+    const videoFilePath = req.body.videoFilePath
+    const outputFilePath = req.body.outputFilePath
+    
+    if (!videoFilePath || !outputFilePath){
+        res.status(400).send("Bad request: Missing video file path")
+    }
+
+    ffmpeg(videoFilePath).outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+        res.status(200).send("Video processed successfully");
+    }).on("error", (err) => {
+        return res.status(500).send(`Internal server error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Initial video processing setup, listening at http://localhost:${port}`)
 })
