b2e9213f93755b51096c9660bd87bf1c6cae36fc
Chloe Eimunjeze <45670453+apropos0@users.noreply.github.com>
2023-10-01 13:38:30 -0500

    Add generate upload url and firebase v2 dep

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -2,10 +2,14 @@ import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+const rawVideoBucketName = "ytube-clone-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -18,3 +22,29 @@ export const createUser = functions.auth.user().onCreate((user) => {
   logger.info(`User Creates: ${JSON.stringify(userInfo)}`);
   return;
 });
+
+export const generateUploadUrl = onCall(
+  {maxInstances: 1}, async (request) => {
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated"
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {url, fileName};
+  }
+);
